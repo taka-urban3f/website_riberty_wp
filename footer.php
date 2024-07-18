1fc65ebba90c9a1425b1fb9cceb5bb10ffdeb600
taka <taka@users.noreply.github.com>
2024-07-18 13:45:47 +0900

    開始

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer class="l-footer">
+            <ul class="l-footer__snsBox">
+                <li class="l-footer__li"><a href="#" class="l-footer__snsBox__a"><img src="img/sns_facebook.svg" alt="フェイスブック" class="l-footer__snsBox__img"></a></li>
+                <li class="l-footer__li"><a href="#" class="l-footer__snsBox__a"><img src="img/sns_insta.svg" alt="インスタグラム" class="l-footer__snsBox__img"></a></li>
+                <li class="l-footer__li"><a href="#" class="l-footer__snsBox__a"><img src="img/sns_x.svg" alt="エックス（旧ツイッター）" class="l-footer__snsBox__img"></a></li>
+            </ul>
+
+            <ul class="l-footer__ul">
+                <li class="l-footer__li"><a href="index.html#secLineup" class="l-footer__a">ラインナップ</a></li>
+                <li class="l-footer__li"><a href="index.html#secBlog" class="l-footer__a">店長のつぶやき</a></li>
+                <li class="l-footer__li"><a href="index.html#secNews" class="l-footer__a">ニュース</a></li>
+                <li class="l-footer__li"><a href="index.html#secInformation" class="l-footer__a">インフォメーション</a></li>
+            </ul>
+
+            <p class="l-footer__p">&copy;2024 Guitar Shop Riberty</p>
+        </footer>
+    </main>
+
+    <script src="js/script.js"></script>
+
+    <?php wp_footer(); ?>
+</body>
+
+</html>
